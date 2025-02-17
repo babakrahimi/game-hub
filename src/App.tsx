1496477cdbb05c0100eb21caf683e12e68dfbd13
Babak <babak@Babaks-MacBook-Pro.local>
2025-02-17 18:13:42 +0100

    Modify layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  const showAside = useBreakpointValue({ base: false, lg: true });
-
   return (
     <Grid
       templateAreas={{
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{ base: "1fr", lg: "250px 1fr" }}
       gap={4}
       p={4}
     >
-      <GridItem area="nav" bg="blue.500" p={4} color="white">
+      <GridItem area="nav" bg="colorPalette.50" p={4}>
         Navigation
       </GridItem>
 
-      {showAside && (
-        <GridItem area="aside" bg="gray.200" p={4}>
-          Sidebar
-        </GridItem>
-      )}
-
       <GridItem area="main" bg="green.500" p={4} color="white">
         Main Content
+      </GridItem>
+
+      <GridItem
+        area="aside"
+        display={{ base: "none", lg: "block" }}
+        bg="gray.200"
+        p={4}
+      >
+        Sidebar
       </GridItem>
     </Grid>
   );

8f1957a954761605498ef43f2c2d2dc00a548928
Babak <babak82@gmail.com>
2025-02-21 15:40:08 +0100

    Refactor: use react query to fetch platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -21,7 +21,7 @@ function PlatformSelector({
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}

03331bf01659dd7183c7390c9d4e3ae266766975
Babak <babak82@gmail.com>
2025-02-21 18:24:22 +0100

    Refactor: create new api client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,9 @@
 import { GameQuery } from "@/App";
-import apiClient, { FetchResponse } from "@/services/apiClient";
+import APIClient, { FetchResponse } from "@/services/apiClient";
 import { useQuery } from "@tanstack/react-query";
 import { Platform } from "./usePlatforms";
+
+const apiClient = new APIClient<Game>("/games");
 
 export interface Game {
   id: number;
@@ -16,17 +18,14 @@ const useGames = (gameQuery: GameQuery) =>
   useQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: () =>
-      apiClient
-        .get<FetchResponse<Game>>("/games", {
-          params: {
-            genres: gameQuery.genre?.id,
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-          },
-        })
-        .then((res) => res.data)
-        .catch((error) => error),
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+        },
+      }),
     staleTime: 10 * 60 * 1000, // 10 minutes
     retry: 2, // ✅ Retry twice before failing
     onError: (error) => {
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,8 @@
 import genres from "@/data/genres";
-import apiClient, { FetchResponse } from "@/services/apiClient";
+import APIClient from "@/services/apiClient";
 import { useQuery } from "@tanstack/react-query";
+
+const apiClient = new APIClient<Genre>("/genres");
 
 export interface Genre {
   id: number;
@@ -11,8 +13,7 @@ export interface Genre {
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24hrs
     initialData: { count: genres.length, results: genres },
   });
